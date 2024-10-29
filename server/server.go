7d31fbf87523beb7a2791a1b6ff24afee839e45a
Sascha Andres <sascha.andres@outlook.com>
2024-10-29 07:52:18 +0100

    feat(server): export lockServer

--- server/server.go
+++ server/server.go
@@ -2,25 +2,26 @@ package server
 
 import (
 	"context"
+
 	"github.com/sascha-andres/lockutil/internal/lockmanager"
 
 	pb "github.com/sascha-andres/lockutil/internal/lockserver" // Import the generated proto package
 )
 
-type lockServer struct {
+type LockServer struct {
 	pb.UnimplementedLockServiceServer
 	manager *lockmanager.LockManager
 }
 
-// NewLockServer initializes a new lockServer
-func NewLockServer() *lockServer {
-	return &lockServer{
+// NewLockServer initializes a new LockServer
+func NewLockServer() *LockServer {
+	return &LockServer{
 		manager: lockmanager.NewLockManager(),
 	}
 }
 
 // RequestLock handles lock requests from clients
-func (s *lockServer) RequestLock(ctx context.Context, req *pb.LockRequest) (*pb.LockResponse, error) {
+func (s *LockServer) RequestLock(ctx context.Context, req *pb.LockRequest) (*pb.LockResponse, error) {
 	err := s.manager.RequestLock(req.LockName, req.Pid, req.TimeoutSeconds)
 	if err != nil {
 		return &pb.LockResponse{Success: false, Message: err.Error()}, nil
@@ -29,7 +30,7 @@ func (s *lockServer) RequestLock(ctx context.Context, req *pb.LockRequest) (*pb.
 }
 
 // ReleaseLock handles lock release requests from clients
-func (s *lockServer) ReleaseLock(ctx context.Context, req *pb.ReleaseRequest) (*pb.ReleaseResponse, error) {
+func (s *LockServer) ReleaseLock(ctx context.Context, req *pb.ReleaseRequest) (*pb.ReleaseResponse, error) {
 	err := s.manager.ReleaseLock(req.LockName, req.Pid)
 	if err != nil {
 		return &pb.ReleaseResponse{Success: false, Message: err.Error()}, nil
