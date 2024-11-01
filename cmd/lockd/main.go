5f94e55fb136850e2b91eab1fc3590c7149df211
Sascha Andres <sascha.andres@outlook.com>
2024-11-01 06:17:56 +0100

    feat(lockd): commandline flags

--- cmd/lockd/main.go
+++ cmd/lockd/main.go
@@ -2,21 +2,60 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	pb "github.com/sascha-andres/lockutil/internal/lockserver"
 
-	"log"
 	"net"
 
 	"github.com/sascha-andres/lockutil/server"
+	"github.com/sascha-andres/reuse/flag"
 	"google.golang.org/grpc"
 )
 
+const (
+	defaultPort     = ":50051"
+	defaultHost     = "localhost"
+	applicationName = "lockd"
+)
+
+var (
+	port string
+	host string
+	help bool
+)
+
+// init initializes the logger settings, environment, and command-line flags for the application.
+func init() {
+	log.SetPrefix(fmt.Sprintf("[%s] ", applicationName))
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+	flag.SetEnvPrefix(strings.ToUpper(applicationName))
+
+	flag.StringVar(&port, "port", defaultPort, "The port to listen on")
+	flag.StringVar(&host, "host", defaultHost, "The host to listen on")
+	flag.BoolVar(&help, "help", false, "Prints this help message")
+}
+
+// main is the entry point of the program, handling command-line flag parsing and executing the main functionality.
 func main() {
+	flag.Parse()
+	if help {
+		flag.Usage()
+		return
+	}
+
+	if err := run(); err != nil {
+		log.Fatalf("Failed to run: %v", err)
+	}
+}
+
+// run starts a gRPC server on the default host and port, registers the LockService, and begins serving client requests.
+func run() error {
 	// Set up a listener on port 50051
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s%s", defaultHost, defaultPort))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return err
 	}
 
 	// Create a new gRPC server
@@ -25,8 +64,6 @@ func main() {
 	// Register the lock service
 	pb.RegisterLockServiceServer(grpcServer, server.NewLockServer())
 
-	fmt.Println("gRPC server running on port 50051...")
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	fmt.Printf("gRPC server running on port %q:%q...", host, port)
+	return grpcServer.Serve(lis)
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/sascha-andres/reuse v0.7.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
