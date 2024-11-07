cac864e5fa8ba3290b8e6b26637bc81c7125507e
Sascha Andres <sascha.andres@outlook.com>
2024-11-07 05:45:05 +0100

    feat(lock): implement

--- cmd/lock/testlock.sh
+++ cmd/lock/testlock.sh
@@ -2,11 +2,13 @@
 
 go build -o lock_test main.go
 
-./lock_test -verbose
-./lock_test -verbose -timeout 2
+./lock_test -verbose -port 51001
+./lock_test -verbose -timeout 2 -port 51001
 
-sleep 2
+./lock_test list -port 51001
 
-./lock_test release -verbose
+./lock_test release -verbose -port 51001
+
+./lock_test list -port 51001
 
 rm lock_test
